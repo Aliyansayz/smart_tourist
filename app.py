62a9770d55874039abb3596faba48745bbd63072
Aliyan Anwar <muhammad.aliyan22@gmail.com>
2024-01-04 00:43:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, request, redirect, url_for, jsonify
+from flask import Flask, render_template_string, render_template, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 from response import * 
 
